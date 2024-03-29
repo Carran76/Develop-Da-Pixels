975610f80d597327653c14613fae72a94c3ad6a1
Carran76 <coolcaspian76@gmail.com>
2024-03-30 00:06:59 +0100

    first commit

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll('.nav-link');
-    const contents = document.querySelectorAll('.content');
-  
-    navLinks.forEach(function(navLink) {
-      navLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        const targetId = navLink.getAttribute('data-target');
-        // Dölj alla innehållselement
-        contents.forEach(function(content) {
-          content.style.display = 'none';
-        });
-        // Visa det valda innehållet
-        const targetContent = document.getElementById(targetId);
-        if (targetContent) {
-          targetContent.style.display = 'block';
-        }
-      });
-    });
-});
+    const joinButton = document.querySelector('.home-get-started.button');
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Din befintliga kod
-  
-    const hireLink = document.getElementById('hire-link');
-    hireLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      // Öppna discord.com i ett nytt fönster
-      window.open('https://discordapp.com/users/944959943310737408/', '_blank');
+    // Event lyssnare för att öppna en ny sida när "Join" knappen klickas
+    joinButton.addEventListener('click', function() {
+        // Ange den URL du vill navigera till här
+        const destinationURL = 'https://discord.gg/hgQAgnb32W'; // Ersätt med din faktiska URL
+        // Öppna den nya sidan
+        window.location.href = destinationURL;
     });
 });
-  
-  
